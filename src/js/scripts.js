19e639f49e2fac50e48c4a3617c8280401ac4e7d
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-10-15 11:05:48 +0800

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,20 @@
-// alert('Hi')
+const inputSearch = document.querySelector('.input-search')
+const buttonSearch = document.querySelector('.button-search')
+
+const weatherIcon = document.querySelector('.weather-icon')
+
+const degrees = document.querySelector('.degrees')
+
+const cityName = document.querySelector('.city-name')
+
+const humidity = document.querySelector('#humidity')
+
+
+async function getWeather() {
+
+}
+
+
+// API key a62efa4030ce598957e7558850818388
+
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
